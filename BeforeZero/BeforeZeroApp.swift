8c6411e7829854148c30d864411c8aa463136809
acortino <hello@acortino.me>
2026-01-27 17:34:12 +0100

    Initial Commit

---
+++ BeforeZero/BeforeZeroApp.swift
@@ -0,0 +1,32 @@
+//
+//  BeforeZeroApp.swift
+//  BeforeZero
+//
+//  Created by acortino on 27/01/2026.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct BeforeZeroApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ BeforeZeroTests/BeforeZeroTests.swift
@@ -0,0 +1,17 @@
+//
+//  BeforeZeroTests.swift
+//  BeforeZeroTests
+//
+//  Created by acortino on 27/01/2026.
+//
+
+import Testing
+@testable import BeforeZero
+
+struct BeforeZeroTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
